6a1a3c597912d3c42d886a4897e6946dc3c4f81f
caio <kaio.braga98@gmail.com>
2020-04-14 11:42:11 -0300

    component criar lembrete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,21 +8,27 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ErrorMsgComponent } from './compartilhado/error-msg/error-msg.component';
 import { ListaLembreteComponent } from './paginas/lista-lembrete/lista-lembrete.component';
+import { FormLembreteComponent } from './compartilhado/form-lembrete/form-lembrete.component';
+import { FormsModule } from '@angular/forms';
+import { CriarLembreteComponent } from './paginas/criar-lembrete/criar-lembrete.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ErrorMsgComponent,
     ListaLembreteComponent,
-    
+    FormLembreteComponent,
+    CriarLembreteComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatSliderModule
-    
+    MatSliderModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
